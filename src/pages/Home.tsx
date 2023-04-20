aa931751c6012f05d4e042d4456ec220e024be2d
Atlon1 <maciej.szajstek@gmail.com>
2023-04-20 21:48:59 +0200

    Banner section rdy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Banner from "../components/Banner";
+
+
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className='min-h-[1800px]'>
+            <Banner/>
         </div>
     );
 };
