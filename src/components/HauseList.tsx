5501ef6b61cd6e4209e884b4d19888f34bbe16b4
Atlon1 <maciej.szajstek@gmail.com>
2023-05-04 20:49:02 +0200

    section House list rdy

--- src/components/HauseList.tsx
+++ src/components/HauseList.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useContext} from 'react';
+
+import {HouseContext} from "./HauseContext";
+import Haouse from "./Haouse";
+import {Link} from "react-router-dom"
+import {ImSpinner2} from "react-icons/im"
+
 
 const HauseList = () => {
+    // @ts-ignore
+    const {houses , loading } = useContext(HouseContext)
+
+
     return (
-        <div>
-            HauseList
-        </div>
+        <section className='mb-20'>
+            <div className='container mx-auto'>
+                <div>
+                    {houses.map((house : any, index: number)=> {
+                        return (
+                            <Link to={`/property/${house.id}`} key={index}>
+                                <Haouse house={house}/>
+                            </Link>
+                        )
+                    })}
+                </div>
+            </div>
+        </section>
     );
 };
 
