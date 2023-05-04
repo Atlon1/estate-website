29f354efb252cff9866493e4d28a2dec0678a26d
Atlon1 <maciej.szajstek@gmail.com>
2023-05-04 21:52:34 +0200

    section houseList rdy

--- src/components/HauseContext.tsx
+++ src/components/HauseContext.tsx
@@ -3,16 +3,16 @@ import {housesData} from "../data";
 import {HouseContextType} from "../@types/context";
 
 
-export const HouseContext = createContext<HouseContextType | null> (null) as any
+export const HouseContext = createContext<HouseContextType | null>(null) as any
 
-const HouseContextProvider = ({children} : any) => {
+const HouseContextProvider = ({children}: any) => {
     const [houses, setHouses] = useState(housesData)
     const [country, setCountry] = useState('Location (any)')
     const [countries, setCountries] = useState([])
     const [property, setProperty] = useState('Property type (any)')
     const [properties, setProperties] = useState([])
     const [price, setPrice] = useState('Price range (any)')
-    const [loading, setLoadinf] = useState(false)
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         const allCountries = houses.map((house) => {
@@ -20,10 +20,10 @@ const HouseContextProvider = ({children} : any) => {
         })
 
         // @ts-ignore
-        const uniqueCountries =['Location (any)', ...new Set(allCountries)] as any
+        const uniqueCountries = ['Location (any)', ...new Set(allCountries)] as any
 
         setCountries(uniqueCountries)
-    },[])
+    }, [])
 
     useEffect(() => {
         const allProperties = houses.map((house) => {
@@ -31,13 +31,62 @@ const HouseContextProvider = ({children} : any) => {
         })
 
         // @ts-ignore
-        const uniqueProperties =['Location (any)', ...new Set(allProperties)] as any
+        const uniqueProperties = ['Location (any)', ...new Set(allProperties)] as any
 
         setProperties(uniqueProperties)
-    },[])
+    }, [])
 
     const handleClick = () => {
-        console.log("clicked")
+
+        setLoading(true)
+
+        const isDefault = (str: any) => {
+            return str.split(' ').includes('(any)')
+        }
+        const minPrice = parseInt(price.split(' ')[0])
+        const maxPrice = parseInt(price.split(' ')[2])
+
+        const newHouses = housesData.filter((house: any) => {
+            const housePrcie = parseInt(house.price)
+
+            if (house.country === country &&
+                house.type === property &&
+                housePrcie >= minPrice &&
+                housePrcie <= maxPrice) {
+                return house
+            }
+            if (isDefault(country) && isDefault(property) && isDefault(price)){
+                return house
+            }
+            if (!isDefault(country) && isDefault(property) && isDefault(price)){
+                return house.country === country
+            }
+            if (!isDefault(property) && isDefault(country) && isDefault(price)){
+                return house.type === property
+            }
+            if (!isDefault(price) && isDefault(country) && isDefault(property)){
+                if (housePrcie >= minPrice && housePrcie <= maxPrice){
+                    return house
+                }
+            }
+            if (!isDefault(country) && !isDefault(property) && isDefault(price)){
+                return house.country === country && house.type === property
+            }
+            if (!isDefault(country) && isDefault(property) && !isDefault(price)){
+                if (housePrcie >= minPrice && housePrcie <= maxPrice){
+                    return house.country === country
+                }
+            }
+            if(isDefault(country) && !isDefault(property) && !isDefault(price)){
+                if (housePrcie >= minPrice && housePrcie <= maxPrice){
+                    return house.type === property
+                }
+            }
+        })
+        setTimeout (()=>{
+            return newHouses.length < 1 ? setHouses([]) : setHouses(newHouses),
+            setLoading(false)
+        }, 1000)
     }
 
     return (
@@ -52,7 +101,7 @@ const HouseContextProvider = ({children} : any) => {
             setPrice,
             houses,
             loading,
-            handleClick
+            handleClick,
         }}>
             {children}
         </HouseContext.Provider>
