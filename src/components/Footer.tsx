118c1c8ed307f29211dc0d85a351318bbe71cdb4
Atlon1 <maciej.szajstek@gmail.com>
2023-05-05 22:06:09 +0200

    section footer rdy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Logo from "../assets/Maciej_logo.png"
 
 const Footer = () => {
+
     return (
-        <div>
-            Footer
-        </div>
+        <footer className='bg-black py-9 text-center text-white'>
+            <div className='container mx-auto '>
+                <div className='flex flex-col items-center'>
+                    CopyRight &copy; 2023. All rights reserved.
+                    <img className='w-[200px] py-2' src={Logo}/>
+                </div>
+            </div>
+        </footer>
     );
 };
 
